79f802904bbe9d8af8f16b14b76f865065669a7b
parksunovo <sun930325@gmail.com>
2026-08-16 18:07:17 +0900

    docs: 사용하지 않는 주석 제거

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -12,8 +12,6 @@ public class DiscodeitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DiscodeitApplication.class, args);
 	}
-	UserCreateRequest userCreateRequest = new UserCreateRequest("codeit", "codeit", "codeit.com");
-	UserCreateRequest userCreateRequest2 = new UserCreateRequest("codeit2", "codeit2", "codeit.com");
-	UserCreateRequest userCreateRequest3 = new UserCreateRequest("codeit3", "codeit3", "codeit.com");
+
 
 }
